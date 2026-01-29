9fba48bb0e93ab026e7dc0254e194013d29a7625
SmithaN-Cybersecurity <smitha092004@gmail.com>
2026-01-29 15:47:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("password").addEventListener("input", checkStrength);
+
+function checkStrength() {
+    let password = document.getElementById("password").value;
+    let result = document.getElementById("result");
+    let strength = 0;
+
+    if (password.length >= 8) strength++;
+    if (/[A-Z]/.test(password)) strength++;
+    if (/[a-z]/.test(password)) strength++;
+    if (/[0-9]/.test(password)) strength++;
+    if (/[@$!%*?&]/.test(password)) strength++;
+
+    if (strength <= 2) {
+        result.textContent = "Weak Password ❌";
+        result.style.color = "red";
+    } else if (strength <= 4) {
+        result.textContent = "Medium Password ⚠️";
+        result.style.color = "orange";
+    } else {
+        result.textContent = "Strong Password ✅";
+        result.style.color = "green";
+    }
+}
